9f800d9a39a299c67d61236959132e6adaeca36e
NumberEater <reddickgrant@gmail.com>
2023-10-26 21:59:28 -0400

    Started resources and tools layout

--- app/src/main/java/com/numbereater/investmentapp/ResourcesToolsActivity.kt
+++ app/src/main/java/com/numbereater/investmentapp/ResourcesToolsActivity.kt
@@ -2,10 +2,24 @@ package com.numbereater.investmentapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
 
 class ResourcesToolsActivity : AppCompatActivity() {
+    private lateinit var websiteLinkViews: Array<TextView>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resources_tools)
+
+        websiteLinkViews = arrayOf<TextView>(
+            findViewById(R.id.investopedia_link)
+        )
+        makeWebsiteLinksClickable()
+    }
+
+    private fun makeWebsiteLinksClickable() {
+        for (link in websiteLinkViews) {
+            link.movementMethod = LinkMovementMethod.getInstance()
+        }
     }
 }
