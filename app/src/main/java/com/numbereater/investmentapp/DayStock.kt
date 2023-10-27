557f9f42c23640b2ff74fb198c7a293d83ff01a4
Grant Reddick <reddickgrant@gmail.com>
2023-10-27 01:48:25 -0400

    Bashed my head against the wall for 4 hours

---
+++ app/src/main/java/com/numbereater/investmentapp/DayStock.kt
@@ -0,0 +1,10 @@
+package com.numbereater.investmentapp
+
+data class DayStock(
+    val date: String,
+    val open: String,
+    val high: String,
+    val low: String,
+    val close: String,
+    val volume: String
+)
--- app/src/main/java/com/numbereater/investmentapp/StocksFragment.kt
+++ app/src/main/java/com/numbereater/investmentapp/StocksFragment.kt
@@ -1,18 +1,65 @@
 package com.numbereater.investmentapp
 
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.FrameLayout
+import android.widget.Toast
+import com.google.gson.JsonObject
 
 class StocksFragment : BottomNavigationFragment() {
     override val bottomNavigationButtonId = R.id.stocks_action
+
+    private lateinit var tickerSymbolInput: EditText
+    private lateinit var fragmentContainer: FrameLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stocks, container, false)
+        val layout = inflater.inflate(R.layout.fragment_stocks, container, false)
+        initLayoutElements(layout)
+
+        setTickerInputAction()
+
+        return layout
+    }
+
+    private fun initLayoutElements(layout: View) {
+        tickerSymbolInput = layout.findViewById(R.id.ticker_symbol_input)
+        fragmentContainer = layout.findViewById(R.id.stock_info_fragment_container)
+    }
+
+    private fun setTickerInputAction() {
+        tickerSymbolInput.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                tickerInputAction()
+            }
+            false
+        }
+    }
+
+    private fun tickerInputAction() {
+        Thread {
+            Looper.prepare()
+            val data = StockDataInterface().getDailyFromTicker(tickerSymbolInput.text.toString())
+            if (data != null) {
+                displayData(data)
+            } else {
+                Toast.makeText(context, "Failed to find symbol.", Toast.LENGTH_SHORT).show()
+            }
+        }.start()
+    }
+
+    private fun displayData(data: DailyPriceResponse) {
+        val transaction = childFragmentManager.beginTransaction()
+        transaction.replace(R.id.stock_info_fragment_container, StockDataFragment(data))
+        transaction.commit()
     }
 }
